bb62a7694a629efccb81a8da7e6984e718fa48c4
Bangerzzz <kaci-chaouche.omar@hotmail.com>
2022-05-04 13:23:10 +0200

    [Q3] Change message while changing status codes/methode

--- html.js
+++ html.js
@@ -2,9 +2,14 @@ const http = require("http");
 const host = 'localhost';
 const port = 5000;
 const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "text/html");
-    res.writeHead(200);
-    res.end(`<html><body><h1>HELLO WORLD Omar !</h1></body></html>`);
+    if (req.method=="GET"){
+        res.writeHead(200,{"Content-Type": "text/html; charset=utf-8"});
+        res.end(`<html><body><h1>HELLO WORLD Omar !</h1></body></html>`);
+    }
+    else{
+        res.writeHead(404,{"Content-Type": "text/html; charset=utf-8"})
+        res.end(`<h1>404 Méthode non authorisée</h1>`);
+    }
 };
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
