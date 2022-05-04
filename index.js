3f2484f658c9cc67512080b5b6dbb035d23c36fe
Bangerzzz <kaci-chaouche.omar@hotmail.com>
2022-05-04 15:46:18 +0200

    [Q7]Images et [Q8]Stylecss

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ const requestListener = function (req, res) {
             res.write(fs.readFileSync(path.join(__dirname, "public", "pages", "not_allowed.html")));
             res.end();
         }
+        else if (req.url=="/public/images/image.png" && req.method=="GET"){
+            res.writeHead(200, { "Content-type": 'image/png' });
+            res.write(fs.readFileSync(path.join(__dirname, "public", "images", "image.png")));
+            res.end();
+        }
+        else if (req.url=="/public/css/style.css" && req.method=="GET"){
+            res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+            res.write(fs.readFileSync(path.join(__dirname, "public", "css", "style.css")));
+            res.end();
+        }
         else {
             res.writeHead(404,{"Content-Type": "text/html; charset=utf-8"})
             res.write(fs.readFileSync(path.join(__dirname, "public", "pages", "not_found.html")));
