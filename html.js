45a45769c06b4257238221c0814f572a28dbefcb
Bangerzzz <kaci-chaouche.omar@hotmail.com>
2022-05-04 13:34:26 +0200

    Correction erreur Q3 + [Q4] 404 Not Found

--- html.js
+++ html.js
@@ -2,13 +2,17 @@ const http = require("http");
 const host = 'localhost';
 const port = 5000;
 const requestListener = function (req, res) {
-    if (req.method=="GET"){
+    if (req.method=="GET" && req.url=="/"){
         res.writeHead(200,{"Content-Type": "text/html; charset=utf-8"});
         res.end(`<html><body><h1>HELLO WORLD Omar !</h1></body></html>`);
     }
-    else{
+    else if (req.method!="GET" && req.url=="/"){
+        res.writeHead(401,{"Content-Type": "text/html; charset=utf-8"})
+        res.end(`<h1>401 Méthode non authorisée</h1>`);
+    }
+    else {
         res.writeHead(404,{"Content-Type": "text/html; charset=utf-8"})
-        res.end(`<h1>404 Méthode non authorisée</h1>`);
+        res.end(`<h1>404 Not Found</h1>`)
     }
 };
 const server = http.createServer(requestListener);
